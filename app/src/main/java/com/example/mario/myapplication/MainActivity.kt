9cfaf7a25510510f0ecdf137c7a099766cffb5eb
Pariah Reyes <reyesdelgadomario@gmail.com>
2019-01-18 23:22:17 -0600

    Intents

--- app/src/main/java/com/example/mario/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/mario/myapplication/MainActivity.kt
@@ -19,8 +19,9 @@ class MainActivity : AppCompatActivity() {
         btnSendData.setOnClickListener{
 
             val message = etUserMessage.text.toString()
-            Toast.makeText(this,message,Toast.LENGTH_SHORT).show()
+            //Toast.makeText(this,message,Toast.LENGTH_SHORT).show()
             val intent = Intent(this,SecondActivity::class.java)
+            intent.putExtra("user_message",message)
             startActivity(intent)
         }
     }
--- app/src/main/java/com/example/mario/myapplication/SecondActivity.kt
+++ app/src/main/java/com/example/mario/myapplication/SecondActivity.kt
@@ -3,10 +3,18 @@ package com.example.mario.myapplication
 import android.os.Bundle
 
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity: AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+
+        val bundle = intent.extras
+        val msg = bundle!!.getString("user_message")
+        txtUserMessage.text = msg
+        Toast.makeText(this,msg,Toast.LENGTH_SHORT).show()
     }
 }
