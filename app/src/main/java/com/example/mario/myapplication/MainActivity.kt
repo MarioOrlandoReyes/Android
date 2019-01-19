ba1edab638e26c9e1cb7022de2aa91aea04b423b
Pariah Reyes <reyesdelgadomario@gmail.com>
2019-01-18 22:13:45 -0600

    Section two

--- app/src/main/java/com/example/mario/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/mario/myapplication/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         btnShowToast.setOnClickListener{
-            //Log.i("MainActivity","Button was clicked!")
+            Log.i("MainActivity","Button was clicked!")
             Toast.makeText(this,"Button was clicked",Toast.LENGTH_SHORT).show()
         }
     }
